596cd32d3c63d6b4fa4f7967ce66372eccd0ea1e
Paulo Ferreira <paulo.ferreira@outsystems.com>
2017-02-18 15:58:22 +0000

    Changed constrains to consider empty slices

--- pizza_solver.pl
+++ pizza_solver.pl
@@ -12,12 +12,14 @@ encode_ingredient(mushroom, 1).
 new_slice(MaxW, MaxH, slice(X, W, Y, H)) :-
 	% constrain X and W on pizza width
 	X #>= 0,
-	W #>= 1,
+	W #>= 0, % allow empty slices
 	X + W #=< MaxW,
 	% constrain Y and H on pizza height
 	Y #>= 0,
-	H #>= 1,
-	Y + H #=< MaxH.
+	H #>= 0, % allow empty slices
+	Y + H #=< MaxH,
+	% avoid considering all empty slices, just slice(0, 0, 0, 0)
+	(W #= 0 #\/ H #= 0) #==> (X #= 0 #/\ W #= 0 #/\ Y #= 0 #/\ H #= 0).
 	
 constrain_area(MaxPieces, slice(_, W, _, H), Area) :- 
 	Area #= W * H,
@@ -29,10 +31,16 @@ remove_symmetry([S1,S2|Ss]) :-
 	remove_symmetry(S1, S2),
 	remove_symmetry([S2|Ss]).
 	
-remove_symmetry(slice(X1, _, Y1, _), slice(X2, _, Y2, _)) :-
+remove_symmetry(slice(X1, W1, Y1, _), slice(X2, W2, Y2, _)) :-
+	% remove slice order symmetry
 	X1 #=< X2,
-	Y1 #=< Y2.
-	
+	Y1 #=< Y2,
+	% remove empty slice symmetry
+	(X1 #= X2 #/\ Y1 #= Y2) #==> (W1 #=< W2).
+
+constrain_ingredients(_, _, _, slice(0, 0, 0, 0)) :- 
+	% don't place restrictions on empty slices
+	!.	
 constrain_ingredients(Pizza, MinMushroom, MinTomato, Slice) :-
 	pieces_in_slice(Pizza, Slice, Pieces),
 	encode_ingredient(tomato, TomatoValue),
@@ -46,25 +54,25 @@ solve_sample(Slices, TotalSliceArea) :-
 	MinMushroom = 1,
 	MinTomato = 1,
 	MaxPieces = 6,
-	NSlices = 3,
-	solve_pizza(Pizza, MinMushroom, MinTomato, MaxPieces, NSlices, Slices, TotalSliceArea).
+	MaxSlices = 3,
+	solve_pizza(Pizza, MinMushroom, MinTomato, MaxPieces, MaxSlices, Slices, TotalSliceArea).
 	
-solve_pizza(Pizza, MinMushroom, MinTomato, MaxPieces, NSlices, Slices, TotalSliceArea) :-
+solve_pizza(Pizza, MinMushroom, MinTomato, MaxPieces, MaxSlices, Slices, TotalSliceArea) :-
 	% constrain slices
-	constrain_pizza_slices(Pizza, MaxPieces, NSlices, Slices, TotalSliceArea),
+	constrain_pizza_slices(Pizza, MaxPieces, MaxSlices, Slices, TotalSliceArea),
 	% get possible slice combinations, in order of max TotalArea
 	term_variables(Slices, SliceVars),
 	labeling([max(TotalSliceArea)], SliceVars),
 	% validate ingredients
 	maplist(constrain_ingredients(Pizza, MinMushroom, MinTomato), Slices).
 
-constrain_pizza_slices(Pizza, MaxPieces, NSlices, Slices, TotalSliceArea) :-
+constrain_pizza_slices(Pizza, MaxPieces, MaxSlices, Slices, TotalSliceArea) :-
 	% inputs
 	length(Pizza, R),
 	Pizza = [P|Ps],
 	length(P, C),
     % variables
-	length(Slices, NSlices),
+	length(Slices, MaxSlices),
 	maplist(new_slice(C, R), Slices),
     % remove repeated solutions (different slice order)
 	remove_symmetry(Slices),
@@ -73,7 +81,7 @@ constrain_pizza_slices(Pizza, MaxPieces, NSlices, Slices, TotalSliceArea) :-
     % define total slice area range
 	TotalPizzaArea #= R * C,
 	TotalSliceArea in 0..TotalPizzaArea,
-	length(Areas, NSlices),
+	length(Areas, MaxSlices),
 	maplist(constrain_area(MaxPieces), Slices, Areas),
 	sum(Areas, #=, TotalSliceArea).
 	
@@ -102,3 +110,4 @@ pieces_in_line([_|Vs], X, W, CutVs) :-
 	W > 0,
 	Xm1 is X - 1,
 	pieces_in_line(Vs, Xm1, W, CutVs).
+	
